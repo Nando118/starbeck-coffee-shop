7f4be76751f774143082b5237d8b4a8aba863651
Nando118 <fernandoverdysunata0118@gmail.com>
2023-04-21 10:35:05 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Add Border To Navbar If Mouse Scroll
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".navbar").classList.add("navbar-scrolled");
+  } else {
+    document.querySelector(".navbar").classList.remove("navbar-scrolled");
+  }
+}
