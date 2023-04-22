43185ef446e52d2fb68a2b01407dd1e106709d5b
Nando118 <fernandoverdysunata0118@gmail.com>
2023-04-22 08:52:02 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -23,21 +23,3 @@ function scrollFunction() {
     document.querySelector(".navbar").classList.remove("navbar-scrolled");
   }
 }
-
-// Smooth scroll
-// Ambil semua elemen link menuju section
-const navLinks = document.querySelectorAll('a[href^="#"]');
-
-// Loop setiap link dan tambahkan event listener
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    // Hilangkan default behavior dari link
-    e.preventDefault();
-
-    // Ambil target section berdasarkan href dari link
-    const targetSection = document.querySelector(link.getAttribute("href"));
-
-    // Lakukan scroll dengan animasi ke section yang dituju
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
-});
