3df72c6d06e3b117fc47f1906ca82a74308d66cf
Nando118 <fernandoverdysunata0118@gmail.com>
2023-05-06 08:17:40 +0700

    baru sampe menit ke 47:12

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 // Toggle Hamburger
 const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
+// Toggle Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button").onclick = (e) => {
+  const search = document.querySelector("#search-button");
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Remove Toggle Class Jika Di Klik Di Luar Element
 const hamburger = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
 
